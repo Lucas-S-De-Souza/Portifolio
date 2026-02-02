fc6fdf23790c97decc236ad1cb3b7bb264a70ebe
Lucas Santana <lucassantana0503@gmail.com>
2026-02-01 22:55:10 -0300

    fazendo os cards de Habilidades

--- src/components/Sobre/Sobre.jsx
+++ src/components/Sobre/Sobre.jsx
@@ -25,18 +25,48 @@ const Sobre = () => {
 
             <p>
               Embora transite por todo o ecossistema de uma aplicação, concentro
-              minha especialidade no <span>Front-end</span> com <span>React</span> e <span>TypeScript</span>,
-              garantindo tipagem robusta e interfaces dinâmicas. No <span>Back-end</span>,
-              possuo domínio em <span>Node.js</span> e <span>Java</span>, o que me permite construir
-              lógicas seguras e integrações eficientes. 
+              minha especialidade no <span>Front-end</span> com{" "}
+              <span>React</span> e <span>TypeScript</span>, garantindo tipagem
+              robusta e interfaces dinâmicas. No <span>Back-end</span>, possuo
+              domínio em <span>Node.js</span> e <span>Java</span>, o que me
+              permite construir lógicas seguras e integrações eficientes.
               <br />
-              Além disso, minha base
-              em <span>SQL</span> e <span>Kotlin</span> amplia minha visão sobre gestão de dados e o
-              universo mobile. Unindo a teoria da minha formação acadêmica com
-              projetos práticos, busco constantemente consolidar minha evolução
-              como desenvolvedor, entregando aplicações que unem design
-              intuitivo a uma arquitetura de bastidores sólida.
+              Além disso, minha base em <span>SQL</span> e <span>Kotlin</span>{" "}
+              amplia minha visão sobre gestão de dados e o universo mobile.
+              Unindo a teoria da minha formação acadêmica com projetos práticos,
+              busco constantemente consolidar minha evolução como desenvolvedor,
+              entregando aplicações que unem design intuitivo a uma arquitetura
+              de bastidores sólida.
             </p>
+
+            <div className={styles.skillsContainer}>
+              <div className={styles.skillCard}>
+                <i className="bx bx-code-alt"></i>
+                <h3>Front-end</h3>
+                <span className={styles.subCard}>HTML</span>
+                <span className={styles.subCard}>CSS</span>
+                <span className={styles.subCard}>Typescript</span>
+                <span className={styles.subCard}>React</span>
+              </div>
+
+              <div className={styles.skillCard}>
+                <i className="bx bx-server"></i>
+                <h3>Back-end & Mobile</h3>
+                <span className={styles.subCard}>Node.js</span>
+                <span className={styles.subCard}>Kotlin</span>
+                <span className={styles.subCard}>Java</span>
+                <span className={styles.subCard}>C#</span>
+              </div>
+
+              <div className={styles.skillCard}>
+                <i className="bx bx-wrench"></i>
+                <h3>Ferramentas & DB</h3>
+                <span className={styles.subCard}>Git</span>
+                <span className={styles.subCard}>GitHub</span>
+                <span className={styles.subCard}>Firebase</span>
+                <span className={styles.subCard}>SQL</span>
+              </div>
+            </div>
           </div>
         </div>
 
